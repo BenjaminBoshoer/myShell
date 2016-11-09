007016132cab33091e1b694a27f611353f69d820
Benjamin Boshoer <boshoer.beny@gmail.com>
2016-11-09 22:06:59 +0200

    Test Commit

--- Shell.c
+++ Shell.c
@@ -1,26 +1,35 @@
 #include <stdio.h>
+#include "LineParser.h"
 #include <unistd.h>
 #include <linux/limits.h>
 #include <string.h>
+
+
+
+void execute(cmdLine *pCmdLine);
+
 
 int main()
 {
     char buffer[PATH_MAX + 1];
     char *cwd = getcwd(buffer, PATH_MAX + 1);
     char currCommand[100];
+    struct cmdLine com;
+
+
+
     while(strcmp(currCommand, "exit()") != 0)
     {
         printf("%s>>", buffer);
         gets(currCommand);
-
-
-
-
+        execute(parseCmdLines(currCommand));
     }        
 
+}
 
-
-
+void execute(cmdLine *pCmdLIne)
+{
+    
 
 
 }
